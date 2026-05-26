6cb9bce5287127dae60e82b2a068a990b89b3ac4
Usuari <ikerguien@gmail.com>
2026-05-26 16:08:41 +0200

    seeder accesorios fotos

--- Ledboys/database/seeders/ItemAccesoriosSeeder.php
+++ Ledboys/database/seeders/ItemAccesoriosSeeder.php
@@ -8,18 +8,32 @@ use Illuminate\Support\Facades\DB;
 
 class ItemAccesoriosSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
+        $carpeta = storage_path('app/Accesorios');
+
         DB::table('item_accesorios')->insert([
-            ['item_id' => 59, 'stock_total' => 10], // Barra Limbo
-            ['item_id' => 60, 'stock_total' => 10], // Pistola de Burbujas
-            ['item_id' => 61, 'stock_total' => 10], // Bengalas LED
+            [
+                'item_id'     => 59,
+                'stock_total' => 10,
+                'imagen'      => file_exists($carpeta . '/BarraLimboLed.jpg')
+                                    ? file_get_contents($carpeta . '/BarraLimboLed.jpg')
+                                    : null,
+            ],
+            [
+                'item_id'     => 60,
+                'stock_total' => 10,
+                'imagen'      => file_exists($carpeta . '/pistola_burbujas.jpg')
+                                    ? file_get_contents($carpeta . '/pistola_burbujas.jpg')
+                                    : null,
+            ],
+            [
+                'item_id'     => 61,
+                'stock_total' => 10,
+                'imagen'      => file_exists($carpeta . '/bengala_led.jpg')
+                                    ? file_get_contents($carpeta . '/bengala_led.jpg')
+                                    : null,
+            ],
         ]);
-        
     }
 }
